09c5623461e3bb52c028c5269bd6b90050a57e99
Demoy <sdjkl5437@126.com>
2021-01-09 05:56:06 +0800

    add check of discharge

--- php/checkNA.php
+++ php/checkNA.php
@@ -34,6 +34,45 @@ else {
     $msg=['success'=>'1'];
     echo json_encode($msg);
 }
-   
+//find most 2 recent  nucleic_acid_testing_result 
+$sql = "select result  from nucleic_acid_testing_result where nucleic_acid_testing_result.patient_ID = $targetID order by result_ID desc limit 2";
+$result = $conn->query($sql);
+$nuc = [];
+while(($row = mysqli_fetch_assoc($result))){
+    array_push($nuc,$row);   
+}
+//find most 3 recent temperature
+$sql = "select temperature from patient_status where patient_ID = $targetID order by record_ID desc limit 3";
+$result = $conn->query($sql);
+$tem = [];
+while(($row = mysqli_fetch_assoc($result))){
+    array_push($tem,$row);   
+}
+//find treatment_area
+$sql = "select treatment_area from patient where patient_ID = $targetID ";
+$result = $conn->query($sql);
+$tre = [];
+while(($row = mysqli_fetch_assoc($result))){
+    array_push($tre,$row);   
+}
+
+if($nuc[0]['result'] =='negative' && $nuc[1]['result'] == 'negative' && (strtotime($nuc[0]['time']) - strtotime($nuc[1]['time']) > 3600000)&& $tre[0]['treatment_area'] == 'mild' && $tem[0]['temperature'] <37.3 && $tem[1]['temperature'] <37.3 && $tem[2]['temperature'] <37.3){
+    //find doctor of mild area
+    $sql = "select user_ID from user where type = 'doctor' and treatment_area = 'mild' ";
+    $result = $conn->query($sql);
+    $row = mysqli_fetch_assoc($result);
+    $doctor_id_mild = $row['user_ID'];
+    $sql = "insert into message (patient_ID, to) values ('$targetID','$doctor_id_mild') ";
+    $result = $conn->query($sql);
+    if(!$result){
+        echo json_encode($sql);
+        echo json_encode($nuc);
+        echo json_encode($tem);
+        echo json_encode($tre);
+        echo json_encode($doctor_id_mild);
+    }
+}
+
+
 mysqli_close($conn);
 ?>
